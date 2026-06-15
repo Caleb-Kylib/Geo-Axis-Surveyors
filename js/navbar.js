46022e8b76a3d9eb5a205778202512cc2e357d13
Your Name <lemayiancaleb18@gmail.com>
2026-06-15 14:22:13 +0300

    new contacts

--- js/navbar.js
+++ js/navbar.js
@@ -9,13 +9,13 @@ const navbarHTML = `
     <div class="container">
         <div class="info-left">
             <span><i class="fa-solid fa-location-dot"></i> Maasai Mall, Ongata Rongai</span>
-            <span><i class="fa-solid fa-phone"></i> +254 700 000 000</span>
+            <span><i class="fa-solid fa-phone"></i> +254 710 887 713</span>
             <span><i class="fa-solid fa-envelope"></i> info@geoaxis.co.ke</span>
         </div>
         <div class="info-right">
             <a href="#" aria-label="LinkedIn"><i class="fa-brands fa-linkedin"></i></a>
             <a href="#" aria-label="Facebook"><i class="fa-brands fa-facebook"></i></a>
-            <a href="https://wa.me/254700000000" aria-label="WhatsApp"><i class="fa-brands fa-whatsapp"></i></a>
+            <a href="https://wa.me/254710887713" aria-label="WhatsApp"><i class="fa-brands fa-whatsapp"></i></a>
         </div>
     </div>
 </div>
